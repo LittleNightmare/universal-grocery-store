eb7e493d374046d9ac5e44719e013d31cd94631f
HW925 <jwjm1075@gmail.com>
2022-03-09 19:42:58 -0500

    Changes for package1

---
+++ js/cart.js
@@ -0,0 +1,201 @@
+let products = [
+    {
+        name: "Lactantia",
+        tag: "lactantia",
+        price: 3.49,
+        incart: 0,
+        img: "image/milk20.JPG",
+    },
+
+    {
+        name: "Horizon",
+        tag: "horizon",
+        price: 4.29,
+        incart: 0,
+        img: "image/wholemilk.JPG",
+    },
+
+    {
+        name: "Parmalat",
+        tag: "parmalat",
+        price: 4.99,
+        incart: 0,
+        img: "image/milk2.JPG",
+    },
+
+    {
+        name: "Nestle Yogurt",
+        tag: "nestle yogurt",
+        price: 3.29,
+        incart: 0,
+        img: "image/yogurt2.JPG",
+    },
+
+    {
+        name: "Organic Eggs",
+        tag: "organic eggs",
+        price: 3.99,
+        incart: 0,
+        img: "image/eggs.JPG",
+    },
+
+    {
+        name: "Gala apples",
+        tag: "gala apples",
+        price: 1.19,
+        incart: 0,
+        img: "image/apples.JPG",
+    },
+
+    {
+        name: "Oranges",
+        tag: "oranges",
+        price: 1.29,
+        incart: 0,
+        img: "image/orange.JPG",
+    },
+
+    {
+        name: "Limes",
+        tag: "limes",
+        price: 2.99,
+        incart: 0,
+        img: "image/lime.JPG",
+    },
+
+    {
+        name: "Bananas",
+        tag: "bananas",
+        price: 0.89,
+        incart: 0,
+        img: "image/banana.JPG",
+    },
+
+    {
+        name: "Peaches",
+        tag: "peaches",
+        price: 2.99,
+        incart: 0,
+        img: "image/peach.JPG",
+    },
+
+    {
+        name: "Tomatoes",
+        tag: "tomatoes",
+        price: 1.29,
+        incart: 0,
+        img: "image/tomato.JPG",
+    },
+
+    {
+        name: "Cauliflower",
+        tag: "cauliflower",
+        price: 2.29,
+        incart: 0,
+        img: "image/chou.JPG",
+    },
+
+    {
+        name: "Sweet Corns",
+        tag: "sweet corns",
+        price: 3.99,
+        incart: 0,
+        img: "image/corn.JPG",
+    },
+
+    {
+        name: "Potatoes",
+        tag: "potatoes",
+        price: 0.89,
+        incart: 0,
+        img: "image/potato.JPG",
+    },
+
+    {
+        name: "Cucumbers",
+        tag: "cucumbers",
+        price: 1.99,
+        incart: 0,
+        img: "image/cucumber.JPG",
+    },
+
+    {
+        name: "Atlantic Salmon",
+        tag: "atlantic salmon",
+        price: 6.49,
+        incart: 0,
+        img: "image/atlanticsalmon.webp",
+    },
+
+    {
+        name: "Pacific Salmon",
+        tag: "pacific salmon",
+        price: 7.29,
+        incart: 0,
+        img: "image/pacificsalmon.jfif",
+    },
+
+    {
+        name: "Cod",
+        tag: "cod",
+        price: 4.99,
+        incart: 0,
+        img: "image/cod.jfif",
+    },
+
+    {
+        name: "Catfish",
+        tag: "catfish",
+        price: 3.29,
+        incart: 0,
+        img: "image/catfish.jfif",
+    },
+
+    {
+        name: "Haddock",
+        tag: "haddock",
+        price: 3.99,
+        incart: 0,
+        img: "image/haddock.jfif",
+    },
+
+    {
+        name: "Frozen Tourtiere Pie",
+        tag: "frozen tourtiere pie",
+        price: 6.49,
+        incart: 0,
+        img: "image/frozen_tourtiere_pie.jpg",
+    },
+
+    {
+        name: "33% Less Salt Bacon",
+        tag: "33% less salt bacon",
+        price: 7.29,
+        incart: 0,
+        img: "image/33-less-salt-bacon.jpg",
+    },
+
+    {
+        name: "Bacon",
+        tag: "bacon",
+        price: 4.99,
+        incart: 0,
+        img: "image/bacon.jpg",
+    },
+
+    {
+        name: "Bacon Cheddar Sausage",
+        tag: "bacon cheddar sausage",
+        price: 4.29,
+        incart: 0,
+        img: "image/bacon-cheddar-sausage.jpg",
+    },
+
+    {
+        name: "BBQ Frankfurters",
+        tag: "bbq frankfurters",
+        price: 4.99,
+        incart: 0,
+        img: "image/bbq-frankfurters.jpg",
+    },
+];
