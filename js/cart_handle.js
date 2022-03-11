958214d0ea407446a2ab789678485f37a4a7db3b
LittleNightmare <littlenightmare2000@gmail.com>
2022-03-10 23:46:59 -0500

    add note & optimize logical

--- js/cart_handle.js
+++ js/cart_handle.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
+    // first update summary
     updateSummary()
     
     let cart = localStorage.getItem('Cart');
+    // if there is item in the cart
     if (cart) {
         // create ProductList
         fillProductList();
         $('tbody').on("blur", 'input', function () {
             // update product list after change value
+            // $(this).parent().parent().parent() is current <tr>
             updateQuality($(this).parent().parent().parent(), $(this).val());
         });
     }
 
     //Empty All
     $('.total-price>button.ea').on("click", function () {
+        // TODO animation
         alert("Cart has been CLEAR!");
         localStorage.removeItem("Cart");
         updateSummary();
@@ -22,7 +26,6 @@ $(document).ready(function () {
 
     //Remove button
     $('tbody').on("click", 'button', function(){
-        console.log($(this).parent().find('p').text());
         removeItem($(this).parent().find('p').text());
         updateSummary();
         clearProductList();
@@ -40,7 +43,7 @@ function fillProductList() {
     let cart = localStorage.getItem('Cart');
     let table = $('.cart-table>tbody');
     JSON.parse(cart).forEach(i => {
-        // console.log(i.img, i.name, i.price, i.amount);
+        // add a new row(<tr>)
         var row = newRow(i.img, i.name, i.price, i.amount);
         table.append(row);
     })
@@ -49,6 +52,7 @@ function fillProductList() {
 
 function newRow(img, name, price, amount) {
     var row = document.createElement('tr');
+    // create 3 <td>s separately
     var column1 = document.createElement('td');
     column1.innerHTML = `
     <div id="item">
@@ -83,6 +87,7 @@ function clearProductList() {
 function updateQuality(currentRow, newQuality) {
     let name = currentRow.find("#item>#dis>p").text();
     let cart = JSON.parse(localStorage.getItem('Cart'));
+    
     var change = false;
     cart.forEach(i => {
         if (i.name == name) {
@@ -90,11 +95,9 @@ function updateQuality(currentRow, newQuality) {
                 i.amount = Number(newQuality);
                 change = true;
             }
-
-
         }
     })
-
+    // change only value change
     if (change) {
         localStorage.setItem('Cart', JSON.stringify(cart));
         clearProductList();
@@ -105,22 +108,25 @@ function updateQuality(currentRow, newQuality) {
 };
 
 function updateSummary() {
-
     let cart = localStorage.getItem('Cart');
     let totalItem = 0;
     let subTotal = 0;
+    let QST = 0;
+    let GST = 0;
+    let total = 0;
+
     if(cart){
         cart = JSON.parse(cart);
         cart.forEach(function (good) {
             totalItem = Number(totalItem) + Number(good.amount);
             subTotal = Number(subTotal) + Number(good.price)*100*Number(good.amount);
         });
+
+        subTotal = subTotal/100;
+        QST = parseFloat(((subTotal*100) * 9975 /100000 / 100).toPrecision(12)).toFixed(2);
+        GST = parseFloat((subTotal*100 * 5/10000).toPrecision(12)).toFixed(2);
+        total = parseFloat((subTotal + Number(QST) + Number(GST)).toPrecision(12));
     }
-    
-    subTotal = subTotal/100;
-    let QST = parseFloat(((subTotal*100) * 9975 /100000 / 100).toPrecision(12)).toFixed(2);
-    let GST = parseFloat((subTotal*100 * (0.05*100)/10000).toPrecision(12)).toFixed(2);
-    let total = parseFloat((subTotal + Number(QST) + Number(GST)).toPrecision(12));
     // write in page
     let summary = $('.total-price>table');
     summary.find('.totalitems').text(totalItem);
@@ -131,17 +137,20 @@ function updateSummary() {
 };
 
 function removeItem(name){
+    // only called when exist good in the cart
     let cart = localStorage.getItem('Cart');
     if(cart){
         cart = JSON.parse(cart);
         cart = cart.filter(function(item){
             return item.name !== name;
         });
+
+        if(cart.length == 0){
+            localStorage.removeItem('Cart');
+        } else{
+            localStorage.setItem("Cart",JSON.stringify(cart));
+        }
     }
-    if(cart.length == 0){
-        localStorage.removeItem('Cart');
-    } else{
-        localStorage.setItem("Cart",JSON.stringify(cart));
-    }
+    
     
 };
